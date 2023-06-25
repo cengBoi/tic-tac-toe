21c2683489edc5348e3a746a750d1b11ed731e2c
Yigit Can Akcay <86514747+cengBoi@users.noreply.github.com>
2023-06-25 19:56:06 +0300

    Grid is ready.
    Grid was created by using "-" in two-dimensional list.

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 # Print game grid
-
+grid = [["-" for x in range(3)] for y in range(3)]
+for line in grid:
+    for x in line:
+        print(x, end=" ")
+    print()
 # Take user input
 
 # Check for the win or tie
