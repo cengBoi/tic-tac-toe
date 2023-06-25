48548ee433b0ee09b697a0504f8003813ab36750
Yigit Can Akcay <86514747+cengBoi@users.noreply.github.com>
2023-06-25 22:45:27 +0300

    Taking user input is updated.
    Some missing parts are added to the program.

--- main.py
+++ main.py
@@ -1,21 +1,44 @@
+user_type = "X"
+
 # Print game grid
 grid = ["-" for x in range(9)]
-for index, value in enumerate(grid):
-    print(value, end=" ") if index % 3 != 2 else print(value)
+
+
+def display_grid():
+    for index, value in enumerate(grid):
+        print(value, end=" ") if index % 3 != 2 else print(value)
+
+
+display_grid()
+
 
 # Take user input
-userSelection = -1
-counter = 1
-while userSelection > 9 or userSelection < 1:
-    try:
-        userSelection = int(input("Select a number: "))
-        if userSelection > 9 or userSelection < 1:
-            print("Number is not in the range (1-9)!")
-    except ValueError:
-        print("Input is not a number!")
-    counter += 1
 
-print(type(userSelection))
+def take_input():
+    user_selection = -1
+    counter = 1
+    while user_selection > 9 or user_selection < 1:
+        try:
+            user_selection = int(input("Select a number: "))
+            if user_selection > 9 or user_selection < 1:
+                print("Number is not in the range (1-9)!")
+            else:
+                return user_selection
+        except ValueError:
+            print("Input is not a number!")
+        counter += 1
+
+
+def evaluate_input():
+    cell = take_input() - 1
+    while grid[cell] != "-":
+        print("This cell is already occupied!")
+        cell = take_input()
+    grid[cell] = user_type
+    display_grid()
+
+
+evaluate_input()
 # Check for the win or tie
 
 # Switch player
