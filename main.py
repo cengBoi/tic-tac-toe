50f9c7c1723418e45d7ff65b7374d85eab424e9c
Yigit Can Akcay <86514747+cengBoi@users.noreply.github.com>
2023-06-25 19:48:44 +0300

    Blueprint of the project.
    Steps are written in the main.py file by using comment lines.

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
+# Print game grid
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+# Take user input
 
+# Check for the win or tie
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+# Switch player
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Check for the win or tie again
